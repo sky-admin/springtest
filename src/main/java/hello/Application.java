bbc5bb138ff2b0ecb043e307087c0c017f1875c8
skyADMIN <546485299@qq.com>
2015-12-13 20:39:23 +0800

    Detecting a Device

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,15 +2,13 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by skyADMIN on 15/12/13.
  */
 @SpringBootApplication
-@EnableScheduling
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class,args);
     }
 }
