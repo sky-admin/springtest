8628148f623ce69c560a99a0a35dc02f3f5f9d98
skyADMIN <546485299@qq.com>
2015-12-13 20:28:46 +0800

    Scheduling Tasks

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,13 +2,15 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by skyADMIN on 15/12/13.
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class);
     }
 }
