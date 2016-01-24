7897c696100ea06fb0fffc0bef264a1e1af695a6
skyADMIN <546485299@qq.com>
2016-01-24 21:07:14 +0800

    Building a RESTful Web Service with Spring Boot Actuator

---
+++ src/main/java/hello/HelloWorldConfiguration.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by skyADMIN on 16/1/24.
+ */
+@SpringBootApplication
+public class HelloWorldConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldConfiguration.class,args);
+    }
+}
